5557b733e63a45926d2e27b18aa2040550370efd
souldssa <32550514+souldssa@users.noreply.github.com>
2017-10-07 16:12:53 +0700

    Add content to twig

--- controllers/new.controller.js
+++ controllers/new.controller.js
@@ -1,18 +1,19 @@
 exports.index = function(req, res) {
-	let content = {
-		news: [
-			{
-				id: 1,
-				url: '//image/img-1.jpg' ,
-				title: 'new 1'
-			},
-			{
-				id: 2,
-				url: '//image/img-1.jpg' ,
-				title: 'new 2'
-			}
-		]
-	}
+	// let content = {
+	// 	news: [
+	// 		{
+	// 			id: 1,
+	// 			url: '//image/img-1.jpg' ,
+	// 			title: 'new 1'
+	// 		},
+	// 		{
+	// 			id: 2,
+	// 			url: '//image/img-1.jpg' ,
+	// 			title: 'new 2'
+	// 		}
+	// 	]
+	// }
 
-	res.json(content)
+	// res.json(content)
+	res.render('news.twig')
 }
